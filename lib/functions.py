ca7a535ea5abcf9ca5b184d8f63bb3547be26394
Shukri Hussein <husseinshukri@outlook.com>
2024-03-31 12:56:48 -0400

    lab-complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    print(45 + 55)
+    return num1 + num2
 
 def halve(number):
-    pass
+    print(100 == 50)
+    return number / 2
